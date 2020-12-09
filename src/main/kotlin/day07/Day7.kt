e31ee4c6dec9c31a338e659d02ae4b680d2db009
Oskar Olsson <oskar94olsson@gmail.com>
2020-12-09 14:45:27 +0100

    Day7 cleanup

--- src/main/kotlin/day07/Day7.kt
+++ src/main/kotlin/day07/Day7.kt
@@ -5,64 +5,37 @@ import java.lang.IllegalArgumentException
 
 fun main() {
     val input = InputUtils.getStrings("day07.txt")
-    val day7 = Day7()
-    println(day7.part1(input))
-    println(day7.part2(input))
+    val day7 = Day7(input)
+    println(day7.part1())
+    println(day7.part2())
 }
 
 // Works, but should be a better way to solve this
-class Day7 {
+class Day7(input: List<String>) {
 
-    private var bags = mapOf<String, Bag>()
-    private var containsShiny = mutableSetOf<String>()
+    private var bags = mapBags(input)
 
-    fun part1(inputs: List<String>): Int {
-        var count = 0
-        mapBags(inputs)
-        for (bag in bags.values) {
-            if(canHoldShiny(bag)) {
-                containsShiny.add(bag.color)
-                count++
-            }
-        }
-        return count
+    fun part1(): Int {
+        return bagsHolding("shiny gold").size - 1
     }
 
-    /**
-     * Check bags recursively, adding to a global state to avoid some of the extra checks
-     */
-    private fun canHoldShiny(bag: Bag): Boolean {
-        if (bag.inside.contains("shiny gold")) {
-            return true
-        }
-        for (color in bag.inside.keys) {
-            if (containsShiny.contains(color)) {
-                return true
-            }
-        }
-        return bag.inside.keys
-            .map { color -> bags[color] }
-            .filter { containedBag -> canHoldShiny(containedBag!!) }
-            .any()
+    private fun bagsHolding(bag: String): Set<String> {
+        return bags.values
+            .filter { it.inside.containsKey(bag)}
+            .map { it.color }
+            .flatMap { bagsHolding(it) }
+            .toSet() + bag
     }
 
-    /**
-    * Count all the bags inside the shiny (therefore removing 1 from result)
-    */
-    fun part2(inputs: List<String>): Int {
-        mapBags(inputs)
-        val shiny = bags["shiny gold"] ?: throw IllegalArgumentException("No shiny bag :(")
-        return countInside(shiny) - 1
+    fun part2(): Int {
+        return countInside(getBag("shiny gold"))
     }
 
     private fun countInside(bag: Bag): Int {
-        if (bag.inside.isEmpty()) {
-            return 1
-        }
         return bag.inside
-            .map { colorToAmount -> colorToAmount.value to bags[colorToAmount.key] }
-            .map { amountToBag -> amountToBag.first * countInside(amountToBag.second!!) }
-            .sum() + 1
+            .filter { bag.inside.isNotEmpty() }
+            .map { colorAndAmount -> colorAndAmount.value * (1 + countInside(getBag(colorAndAmount.key))) }
+            .sum()
     }
 
     private fun parseRule(input: String): Bag {
@@ -72,22 +45,26 @@ class Day7 {
         return Bag(color = "${bag[0]} ${bag[1]}", contains)
     }
 
-    private fun parseContaining(contains: String): Map<String, Int> {
-        if (contains.contains("no other")){
+    private fun parseContaining(containing: String): Map<String, Int> {
+        if (containing.contains("no other")){
             return emptyMap()
         }
-        return contains.split(",")
+        return containing.split(",")
             .map { bag ->
                 val words = bag.trim().split(" ")
                 "${words[1]} ${words[2]}" to words[0].toInt()
             }.toMap()
     }
 
-    private fun mapBags(inputs: List<String>) {
-        bags = inputs
+    private fun mapBags(inputs: List<String>): Map<String, Bag> {
+        return inputs
             .map { parseRule(it) }
             .associateBy { bag -> bag.color }
             .toMap()
+    }
+
+    private fun getBag(color: String): Bag {
+        return bags[color] ?: throw IllegalArgumentException("No bag of color: $color")
     }
 
     data class Bag(
